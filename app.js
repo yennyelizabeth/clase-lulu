322c553ea981f4bbe340d9804ccfff8548c019da
yennyelizabeth <yenny.elizabeth@hotmail.com>
2017-11-13 18:21:43 -0500

    añadiendo mas

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var sectionDivs = document.querySelector("section");
 var pushme = function () {
   var div = document.createElement("div"); /*creo div*/
   sectionDivs.appendChild(div); /* lo agrego como hijo */
-  div.setAtribute("style","background:pink; color:blue; height:100px; width:100px; display:inline; padding:4px; ");
+  div.setAtribute("style";"background:pink; color:blue; height:100px; width:100px; display:inline; padding:4px; ");
   var contenido = document.querySelectorAll("div"); /* lista coleccion de nodos*/
   var cantidad = document.length; /* le da el nro */
   var dato = document.createTextNode(cantidad);
